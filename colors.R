90d9ef271aaa16184f7ea32b00d6a6712bb251c0
Austin Pearce <awprc@pm.me>
2022-08-25 11:45:43 -0400

    Keep viz stuff in one place instead of spread across other repos

---
+++ colors.R
@@ -0,0 +1,25 @@
+## Color palettes
+
+# Ever beautiful viridis colors
+library(viridis)
+
+# Similarly beautiful colors for water quality
+library(cmocean)
+
+# Wes Anderson colors
+library(wesanderson)
+
+# Field to Market
+orange_ftm <- "#fd701d"
+blue_ftm <- "#235967"
+
+# NC State University
+red_ncsu <- "#CC0000"
+orange_ncsu <- "#D14905"
+yellow_ncsu <- "#FAC800"
+blue_ncsu <- "#4156A1"
+
+# Atlanta Bravses
+red_braves <- "#CE1141"
+gold_braves <- "#EAAA00"
+blue_braves <- "#13274F"
