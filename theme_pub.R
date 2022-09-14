f7a5e0576131238f8a176540d9c4fb14c93cd6e0
Austin Pearce <awprc@pm.me>
2022-09-14 13:24:53 -0400

    Update theme_pub.R
    options

--- theme_pub.R
+++ theme_pub.R
@@ -27,3 +27,28 @@ theme_pub <-
         legend.justification = c(1, 1))
 
 ggplot2::theme_set(theme_pub)
+
+theme_pub_2 <- 
+    theme_minimal(base_family = serif, base_size = 14) +
+    ggplot2::theme(
+        plot.background = NULL,
+        plot.margin = margin(t = 2, r = 5, b = 2, l = 2, unit = "pt"),
+        panel.grid = element_line(color = "#F4F4F4"),
+        panel.spacing = unit(2, "lines"),
+        panel.border = element_blank(),
+        axis.ticks = element_line(color = "#00000050"),
+        axis.line.y = element_blank(),
+        axis.line.x = element_line(color = "#00000050"),
+        axis.title.y = element_text(
+            hjust = 0.5,
+            margin = margin(t = 0, r = 10, b = 0, l = 0, unit = "pt")),
+        axis.title.x = element_text(
+            hjust = 0,
+            margin = margin(t = 10, r = 0, b = 0, l = 0, unit = "pt")),
+        axis.text = element_text(family = serif),
+        legend.title.align = 0,
+        legend.key.height = unit(x = 5, units = "mm"),
+        legend.justification = c(1, 1))
+
+ggplot2::theme_set(theme_pub_2)
+
